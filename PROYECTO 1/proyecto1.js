2a3bcd6b1ca1a61b205252df1520e940d991027b
MacSoj <116688101+MacSoj@users.noreply.github.com>
2022-11-24 02:58:58 -0600

    Update proyecto1.js

--- PROYECTO 1/proyecto1.js
+++ PROYECTO 1/proyecto1.js
@@ -1,29 +1,25 @@
-let valor1 = prompt("Ingrese el primer valor");
-let valor2 = prompt("Ingrese el segundo valor");
-let valor3 = prompt("Ingrese el tercer valor");
+var keysAndValues = ({
+    A:1,
+    B:2,
+    C:3
+});
 
+var llaves = Object.keys(keysAndValues);
+var kes=[]
+var values=[]
+var total=[]
 
-function keysAndValues(diccionario){
-     var llaves = Object.keys(diccionario);
-     var kes= []
-     var values= []
-     var total= []
-
-     for(var i=0; i<llaves.length; i++){
-         var llave=llaves[i];
-         kes.push(llaves[i]);
-         values.push(diccionario[llave]);
-         if(llaves.length == i+1){
-             total.push(kes)
-             total.push(values)
-         }
-     }
-     return total
+for(var i=0; i<llaves.length; i++){
+    var llave = llaves[i];
+    kes.push(llaves[i])
+    values.push(keysAndValues[llave])
+    if(llaves.length===i+1){
+        total.push(kes)
+        total.push(values)
+    }
 }
 
-if(typeof document !== 'undefined'){
- document.write(keysAndValues({a:valor1, b:valor2, c:valor3}));
-}
+console.log(total)
 
 
 
